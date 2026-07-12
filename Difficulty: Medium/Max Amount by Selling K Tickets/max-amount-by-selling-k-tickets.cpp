ce4d6f22524d8edbdeff353a653a8b625ab69407
MD SHAHID RAZA <shahidrazaraza77@gmail.com>
2026-07-12 22:41:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Amount by Selling K Tickets/max-amount-by-selling-k-tickets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxAmount(vector<int>& arr, int k) {
+        const int MOD = 1000000007;
+        priority_queue<int> pq;
+
+        for (int x : arr)
+            pq.push(x);
+
+        long long ans = 0;
+
+        while (k-- && !pq.empty()) {
+            int x = pq.top();
+            pq.pop();
+
+            ans = (ans + x) % MOD;
+
+            if (x > 1)
+                pq.push(x - 1);
+        }
+
+        return ans % MOD;
+    }
+};
